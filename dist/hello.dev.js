5768156004ac62c661b49ada87a7780df263049f
Muhammad Mahbubur Rahman <muhammadmahbuburrahman@Muhammads-MacBook-Air.local>
2020-08-29 16:45:12 +0900

    code added

--- dist/hello.dev.js
+++ dist/hello.dev.js
@@ -1,3 +1,16 @@
 "use strict";
 
 console.log('HEllo');
+var classic = 'One';
+console.log(classic); //backup
+
+function add(num1) {
+  var num2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 78;
+  return num1 + num2;
+}
+
+var total = add(13, 7); // If second value is not provided
+
+var newTotal = add(13);
+console.log(total);
+console.log(newTotal);
--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
 console.log('HEllo');
+const classic = 'One';
+
+console.log(classic);
+
+
+//backup
+function add (num1, num2= 78){
+    return num1 + num2;
+
+}
+let total = add(13,7);
+
+// If second value is not provided
+
+let newTotal = add(13);
+console.log(total);
+console.log(newTotal);
